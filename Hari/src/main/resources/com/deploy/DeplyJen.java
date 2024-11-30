277e26e8e63da1638b082bda560e9559ecd719c7
vishwa1-art <maheshwaramshweta1@gmail.com>
2024-11-30 11:35:59 +0530

    Initial commit

---
+++ Hari/src/main/resources/com/deploy/DeplyJen.java
@@ -0,0 +1,10 @@
+package com.deploy;
+
+public class DeplyJen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("deploy in progress");
+	}
+
+}
