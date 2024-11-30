7d96c5dca8346228b57bb74b1a19c6b17f14de5b
vishwa1-art <maheshwaramshweta1@gmail.com>
2024-11-30 11:41:48 +0530

    done

--- Hari/target/classes/com/deploy/DeplyJen.java
+++ Hari/target/classes/com/deploy/DeplyJen.java
@@ -5,6 +5,7 @@ public class DeplyJen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("deploy in progress");
+System.out.println("done");
 	}
 
 }
